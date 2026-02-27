481bd8f4d278f495be6c7e67e4d45dbb1b872394
Enhancements1992 <enhancements@enhancements.eu>
2026-02-27 10:13:00 +0000

    auto-commit for 0d306a5f-971c-495e-a95e-d4664706e98b

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -43,7 +43,7 @@ export default function ContactPage() {
     setSubmitStatus(null);
 
     try {
-      const response = await fetch('https://formspree.io/f/xwpopnpk', {
+      const response = await fetch('https://formspree.io/f/xkovegre', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
